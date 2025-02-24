ed2bcd6c74e560005dc0e7feed0d91bd847fb260
Maithree de Silva <90709583+maithd78@users.noreply.github.com>
2025-02-24 18:37:39 +0900

    completed exercises using array methods

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    if (typeof fib === "string") {
+        var number = parseInt(fib);
+        fib = number;
+    }
 
+    let fibArr = [0,1];
+    for (let i = 2; i <= fib; i++) {
+        let nextSequence = fibArr[i-1] + fibArr[i-2];
+        fibArr.push(nextSequence);
+    }
+
+    if (fib === 0) {
+        return 0;
+    } else if (fib <= -1) {
+        return "OOPS"
+    } else {
+    return fibArr.pop()
+    }
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    arrayAges = [...array].map(function(v) {
+        if (!('yearOfDeath' in v)) {
+            return ({...v, yearOfDeath : "unknown"})
+        }
+        return ({...v})
+    })
 
+    // console.table(arrayAges)
+
+    const sortOldest = arrayAges.sort(function(a,b) {
+
+        last = a.yearOfDeath - a.yearOfBirth;
+        next = b.yearOfDeath - b.yearOfBirth;   
+        if (b.yearOfDeath === "unknown") {
+            next = 2025 - b.yearOfBirth;
+        }
+        return last > next ? -1 : 1
+    })
+    // console.table(arrayAges)
+    console.table(sortOldest)
+    oldest = sortOldest[0]
+
+    return sortOldest[0]
+    
+    
+    
 };
 
 // Do not edit below this line
